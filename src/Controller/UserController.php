251a297110b2d22ebb5c231b5df4bf9e3507213e
OS <goloborodko@inbox.ru>
2021-02-14 20:00:32 +0300

    correct users controller

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,13 +2,17 @@
 
 namespace App\Controller;
 
+use \App\View;
+use \App\Model\User;
+use \App\Model\Subscriber;
+
 class UserController extends PrivateController
 {
     public function index()
     {
     }
 
-    public function new()
+    public function create()
     {
         $error = validateRegistrationData();
 
@@ -20,20 +24,20 @@ class UserController extends PrivateController
         $name = strip_tags($_POST['name']);
         $email = strip_tags($_POST['email']);
 
-        try { \App\Model\User::insert(
+        try { User::insert(
             ['name' => $name, 'email' => $email, 'password' => $password]
             );
         } catch (\Exception $e) {
             throw new \App\Exception\RegistrateException("Невозможно создать пользователя. Такой Email или Имя уже встречается.");
         }
 
-        return new \App\View('registration', ['title' => 'Регистрация пользователя', 'success' => 'Регистрация прошла успешно. Вы можете войти на сайт.']);
+        return new View('registration', ['title' => 'Регистрация пользователя', 'success' => 'Регистрация прошла успешно. Вы можете войти на сайт.']);
     }
 
     public function show($id)
     {
         if (isSession() && $_SESSION['user']->id == $id) {
-            return new \App\View('users.show', ['title' => "Профиль {$_SESSION['user']->name}"]);
+            return new View('users.show', ['title' => "Профиль {$_SESSION['user']->name}"]);
         }
 
         throw new \App\Exception\NotFoundException();
@@ -46,22 +50,21 @@ class UserController extends PrivateController
         }
 
         $validateFileResult = [];
-        $user = \App\Model\User::find($id);
+        $user = User::find($id);
 
         // echo '<pre>';
         // var_dump($user);
         // echo '</pre>';
 
-        if (!empty($_FILES['user-avatar']['name'])) {
-            $validateFileResult = validateFile($_FILES['user-avatar']);
+        $fileUploadResult = validateFile($_FILES['user-avatar']);
+
+        if (isset($fileUploadResult['errors'])) {
+            $error = implode(' ', $fileUploadResult['errors']);
+            throw new \App\Exception\UpdateUserException($error);
         }
 
-        if (isset($validateFileResult['errors'])) {
-            throw new \App\Exception\UpdateUserException(implode(' ', $validateFileResult['errors']));
-        }
-
-        if (isset($validateFileResult['img_src'])) {
-            $user->avatar = $validateFileResult['img_src'];
+        if (isset($fileUploadResult['img_src'])) {
+            $user->avatar = $fileUploadResult['img_src'];
         }
 
         if (isset($_POST['text'])) {
@@ -74,17 +77,17 @@ class UserController extends PrivateController
         if ($result) {
             $_SESSION['user'] = $user;
         }
-        return new \App\View('users.show', ['title' => "Профиль {$_SESSION['user']->name}"]);
+        return new View('users.show', ['title' => "Профиль {$_SESSION['user']->name}"]);
     }
 
     public function registrationGet()
     {
-        return new \App\View('registration', ['title' => 'Регистрация пользователя']);
+        return new View('registration', ['title' => 'Регистрация пользователя']);
     }
 
     public function authorizationGet()
     {
-        return new \App\View('authorization', ['title' => 'Авторизация пользователя']);
+        return new View('authorization', ['title' => 'Авторизация пользователя']);
     }
 
     public function authorizationPost()
@@ -92,10 +95,10 @@ class UserController extends PrivateController
         $email = strip_tags($_POST['email']);
         $password = strip_tags($_POST['password']);
 
-        $user = \App\Model\User::where('email', $email)->first();
+        $user = User::where('email', $email)->first();
 
         if ($user && password_verify($password, $user->password)) {
-            $subscribe = \App\Model\Subscriber::where('email', $user->email)->first();
+            $subscribe = Subscriber::where('email', $user->email)->first();
             $_SESSION['subscribe'] = $subscribe ? 1 : 0;
             $_SESSION['user'] = $user;
             $_SESSION['success'] = true;
